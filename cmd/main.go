6ceb6ba392bf1d91d79344ad151460eccd3c8896
hacker301et <ethiohacking301@gmail.com>
2024-01-20 00:12:54 +0300

    #first integration done

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/hacker301et/live-sub/view"
+)
+
 func main() {
+	m := view.NewView()
+	p := tea.NewProgram(m)
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("could not start program:", err)
+		os.Exit(1)
+	}
 
 }
